b84b752f4e8419eda8ee430f56a5acb1337570a2
Turi-Byte <breldanmuturi001@gmail.com>
2019-12-01 23:28:49 +0300

    AppDatabase Class

---
+++ app/src/main/java/turi/Kotlin/tasktimer/AppDatabase.kt
@@ -0,0 +1,28 @@
+package turi.Kotlin.tasktimer
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+/**
+Basic Database class for the application
+The only class that should use this is the [AppProvider].
+*/
+
+private const val TAG = "AppDatabase"
+private const val  DATABASE_NAME = "TaskTimer.db"
+private const val  DATABASE_VERSION = 3
+internal class AppDatabase constructor (context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+    init {
+        Log.d(TAG, "AppDatabase: initialising")
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
