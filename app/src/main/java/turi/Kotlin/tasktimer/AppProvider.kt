4b24823aaaa18f68fe83e4356ba801ca4d07e492
Turi-Byte <breldanmuturi001@gmail.com>
2019-12-02 22:52:42 +0300

    Registering the Content Provider

--- app/src/main/java/turi/Kotlin/tasktimer/AppProvider.kt
+++ app/src/main/java/turi/Kotlin/tasktimer/AppProvider.kt
@@ -4,6 +4,7 @@ import android.content.ContentProvider
 import android.content.ContentValues
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteQueryBuilder
 import android.net.Uri
 import android.util.Log
 
@@ -11,7 +12,7 @@ import android.util.Log
  * Provider for the TaskTimer App. The only class that knows about [AppDatabase].
  */
 private const val TAG = "AppProvider"
-private val CONTENT_AUTHORITY = "turi.Kotlin.tasktimer.provider"
+const val CONTENT_AUTHORITY = "turi.Kotlin.tasktimer.provider"
 private const val TASKS = 100
 private const val TASKS_ID = 101
 private const val TIMINGS = 200
@@ -19,15 +20,30 @@ private const val TIMINGS_ID = 201
 private const val TASK_DURATIONS = 400
 private const val TASK_DURATIONS_ID = 401
 val CONTENT_AUTHORITY_URI: Uri = Uri.parse("content://$CONTENT_AUTHORITY")
-class AppProvider: ContentProvider() {
+
+class AppProvider : ContentProvider() {
     private val uriMatcher by lazy { buildUriMatcher() }
     private fun buildUriMatcher(): UriMatcher {
-        Log.d(TAG ,"buildUriMatcher starts")
+        Log.d(TAG, "buildUriMatcher: starts")
         val matcher = UriMatcher(UriMatcher.NO_MATCH)
+
+        //e.g content://turi.Kotlin.tasktimer.provider/Tasks
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME, TASKS);
+        //e.g content://turi.Kotlin.tasktimer.provider/Tasks/8
+        matcher.addURI(CONTENT_AUTHORITY, "${TasksContract.TABLE_NAME}/#", TASKS_ID)
+//        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME, TIMINGS);
+//        matcher.addURI(CONTENT_AUTHORITY, "${TimingsContract.TABLE_NAME}/#", TIMINGS_ID)
+//        matcher.addURI(CONTENT_AUTHORITY, DuratoionsContract.TABLE_NAME, TASK_DURATIONS);
+//        matcher.addURI(CONTENT_AUTHORITY, "${DuratoionsContract.TABLE_NAME}/#", TASK_DURATIONS_ID)
         return matcher
     }
 
-    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+    override fun onCreate(): Boolean {
+        Log.d(TAG, "onCreate: starts")
+        return true
+    }
+
+    override fun getType(uri: Uri): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -37,11 +53,48 @@ class AppProvider: ContentProvider() {
         selection: String?,
         selectionArgs: Array<out String>?,
         sortOrder: String?
-    ): Cursor? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    ): Cursor {
+        Log.d(TAG, "query: called with uri $uri")
+        val match = uriMatcher.match(uri)
+        Log.d(TAG, "query: match is $match")
+        val queryBuilder = SQLiteQueryBuilder()
+        when (match) {
+            TASKS -> queryBuilder.tables = TasksContract.TABLE_NAME
+
+            TASKS_ID -> {
+                queryBuilder.tables = TasksContract.TABLE_NAME
+                val taskId = TasksContract.getId(uri)
+                queryBuilder.appendWhereEscapeString("${TasksContract.Columns.ID} = $taskId")       // <-- change method
+            }
+
+//            TIMINGS -> queryBuilder.tables = TimingsContract.TABLE_NAME
+//
+//            TIMINGS_ID -> {
+//                queryBuilder.tables = TimingsContract.TABLE_NAME
+//                val timingId = TimingsContract.getId(uri)
+//                queryBuilder.appendWhereEscapeString("${TimingsContract.Columns.ID} = $timingId")   // <-- and here
+//            }
+//
+//            TASK_DURATIONS -> queryBuilder.tables = DurationsContract.TABLE_NAME
+//
+//            TASK_DURATIONS_ID -> {
+//                queryBuilder.tables = DurationsContract.TABLE_NAME
+//                val durationId = DurationsContract.getId(uri)
+//                queryBuilder.appendWhereEscapeString("${DurationsContract.Columns.ID} = $durationId")   // <-- and here
+//            }
+
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
+        }
+
+        val db = AppDatabase.getInstance(context!!).readableDatabase
+        val cursor =
+            queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder)
+        Log.d(TAG, "query: rows in returned cursor = ${cursor.count}") // TODO remove this line
+
+        return cursor
     }
 
-    override fun onCreate(): Boolean {
+    override fun insert(uri: Uri, values: ContentValues?): Uri {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -55,10 +108,6 @@ class AppProvider: ContentProvider() {
     }
 
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getType(uri: Uri): String? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
